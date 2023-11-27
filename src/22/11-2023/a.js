98336cdd84678d27cf59967b6bf5aba12241edbb
Dance <thakurshivang579@gmail.com>
2023-11-27 22:26:25 +0530

    image editor practice_javascript

--- src/22/11-2023/a.js
+++ src/22/11-2023/a.js
@@ -35,17 +35,76 @@
 //     select.addEventListener('change', ()=>{
 //         select.value=== 'black'? update('black', 'white'): update('white','black');
 //     });
-const button= document.querySelector('body');
+// const button= document.querySelector('.container');
 
-    function random(){
-        return Math.floor(Math.random()*100);
+//     function random(){
+//         return Math.floor(Math.random()*100);
+//     }
+
+// bgchange=(e)=>{
+//     const rndcolor= `#${random()}${random()}${random()}`
+//     e.target.style.backgroundColor= rndcolor;
+// }
+
+// button.addEventListener('click', bgchange);
+// let a=2;
+// while(a <1000){
+//     // let i=0;
+//         if(a == 2){
+//             console.log(a+'&')
+//         }
+//         else if(a%2 == 0 && a%3 == 0){
+//             // console.log('not done')
+//         }
+//         else{
+//             console.log(a+'&')
+//         }
+//     a+=1;
+// }
+
+const display = document.querySelector(".displayed-img");
+const thumbar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector('button');
+const overlay= document.querySelector('.overlay');
+
+    const pic =
+      "https://mdn.github.io/learning-area/javascript/building-blocks/gallery/images";
+    const images = [
+      "/pic1.jpg",
+      "/pic2.jpg",
+      "/pic3.jpg",
+      "/pic4.jpg",
+      "/pic5.jpg",
+    ];
+    const alts = {
+      "pic1.jpg": "Closeup of a human eye",
+      "pic2.jpg": "Rock that looks like a wave",
+      "pic3.jpg": "Purple and white pansies",
+      "pic4.jpg": "Section of wall from a pharoah's tomb",
+      "pic5.jpg": "Large moth on a leaf",
+    };
+
+    for (const image of images) {
+        const newImages = document.createElement('img')
+        newImages.setAttribute('src', `${pic}${image}`);
+        newImages.setAttribute('alt', alts[image]);
+        thumbar.appendChild(newImages);
+        newImages.addEventListener('click', (e)=>{
+            display.src= e.target.src;
+            display.alt= e.target.alt;
+        })
     }
 
-bgchange=(e)=>{
-    const rndcolor= `#${random()}${random()}${random()}`
-    e.target.style.backgroundColor= rndcolor;
-}
-
-button.addEventListener('click', bgchange);
-
-
+btn.addEventListener('click', ()=>{
+    const btnClass= btn.getAttribute('class');
+    if (btnClass === 'dark') {
+        btn.setAttribute('class', 'light')
+        btn.textContent= 'Lighten';
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    }else{
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+});
